4ef0132ab02a90cf5923a1637fb226127386fdde
Rachel Denton <rdenton3@terpmail.umd.edu>
2022-01-18 15:14:35 -0500

    modularize code

--- app.js
+++ app.js
@@ -1,18 +1,13 @@
-const profileDataArgs = process.argv.slice(2, process.argv.length);
-console.log(profileDataArgs);
+const fs = require('fs');
+const generatePage = require('./src/page-template.js');
 
-const printProfileData = profileDataArr => {
-    // This...
-    for (let i = 0; i < profileDataArr.length; i += 1) {
-      console.log(profileDataArr[i]);
-    }
-  
-    console.log('================');
-  
-    // Is the same as this...
-    profileDataArr.forEach((profileItem) => {
-      console.log(profileItem)
-    });
-  };
+const profileDataArgs = process.argv.slice(2);
 
-  profileDataArr.forEach(profileItem => console.log(profileItem));
+const [name, github] = profileDataArgs;
+
+
+fs.writeFile('./index.html', generatePage(name, github), err => {
+  if (err) throw new Error(err);
+
+  console.log('Portfolio complete! Check out index.html to see the output!');
+});
