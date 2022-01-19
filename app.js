2a2aeadb2ab9691e28c55663e40b980edf611d46
Rachel Denton <rdenton3@terpmail.umd.edu>
2022-01-19 15:37:03 -0500

    add in promise functions

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
-const fs = require('fs');
 const inquirer = require('inquirer');
 const generatePage = require('./src/page-template');
+const { writeFile, copyFile } = require('./utils/generate-site');
 
 const promptUser = () => {
   return inquirer.prompt([
@@ -129,23 +129,18 @@ Add a New Project
 promptUser()
   .then(promptProject)
   .then(portfolioData => {
-    const pageHTML = generatePage(portfolioData);
-
-    fs.writeFile('./index.html', pageHTML, err => {
-      if (err) throw new Error(err);
-
-      console.log('Page created! Check out index.html in this directory to see it!');
-    });
+    return generatePage(portfolioData);
+  })
+  .then(pageHTML => {
+    return writeFile(pageHTML);
+  })
+  .then(writeFileResponse => {
+    console.log(writeFileResponse);
+    return copyFile();
+  })
+  .then(copyFileResponse => {
+    console.log(copyFileResponse);
+  })
+  .catch(err => {
+    console.log(err);
   });
-
-// promptUser()
-//   .then(promptProject)
-//   .then(portfolioData => {
-//     console.log(portfolioData);
-//     // will be uncommented in lesson 4
-//     // const pageHTML = generatePage(portfolioData);
-//     // fs.writeFile('./index.html', pageHTML, err => {
-//     //   if (err) throw new Error(err);
-//     //   console.log('Page created! Check out index.html in this directory to see it!');
-//     // });
-//   });
--- src/page-template.js
+++ src/page-template.js
@@ -11,7 +11,8 @@ const generateAbout = aboutText => {
       </section>
     `;
   };
-
+  
+  // create the projects section
   const generateProjects = projectsArr => {
     return `
       <section class="my-3" id="portfolio">
@@ -25,7 +26,7 @@ const generateAbout = aboutText => {
               <h3 class="portfolio-item-title text-light">${name}</h3>
               <h5 class="portfolio-languages">
                 Built With:
-                ${languages.join(', ')}
+                ${languages.map(language => language).join(',')}
               </h5>
               <p>${description}</p>
               <a href="${link}" class="btn"><i class="fab fa-github mr-2"></i>View Project on GitHub</a>
@@ -37,6 +38,7 @@ const generateAbout = aboutText => {
         ${projectsArr
           .filter(({ feature }) => !feature)
           .map(({ name, description, languages, link }) => {
+            console.log(languages);
             return `
             <div class="col-12 col-md-6 mb-2 bg-dark text-light p-3 flex-column">
               <h3 class="portfolio-item-title text-light">${name}</h3>
@@ -50,19 +52,21 @@ const generateAbout = aboutText => {
           `;
           })
           .join('')}
+      
         </div>
       </section>
     `;
   };
-
-module.exports = templateData => {
+  
+  // export function to generate entire page
+  module.exports = templateData => {
     // destructure page data by section
     const { projects, about, ...header } = templateData;
   
     return `
     <!DOCTYPE html>
     <html lang="en">
-  
+    
     <head>
       <meta charset="UTF-8">
       <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -72,24 +76,22 @@ module.exports = templateData => {
       <link href="https://fonts.googleapis.com/css?family=Public+Sans:300i,300,500&display=swap" rel="stylesheet">
       <link rel="stylesheet" href="style.css">
     </head>
-  
+    
     <body>
       <header>
         <div class="container flex-row justify-space-between align-center py-3">
           <h1 class="page-title text-secondary bg-dark py-2 px-3">${header.name}</h1>
           <nav class="flex-row">
-            <a class="ml-2 my-1 px-2 py-1 bg-secondary text-dark" href="https://github.com/${
-              header.github
-            }">GitHub</a>
+            <a class="ml-2 my-1 px-2 py-1 bg-secondary text-dark" href="https://github.com/${header.github}">GitHub</a>
           </nav>
         </div>
       </header>
-    <main class="container my-5">
-    ${generateAbout(about)}
-    ${generateProjects(projects)}
-    </main>
+      <main class="container my-5">
+        ${generateAbout(about)}
+        ${generateProjects(projects)}
+      </main>
       <footer class="container text-center py-3">
-        <h3 class="text-dark">&copy; ${new Date().getFullYear()} by ${header.name}</h3>
+        <h3 class="text-dark">&copy;2020 by ${header.name}</h3>
       </footer>
     </body>
     </html>
